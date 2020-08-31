b4c7c1fef31004fb2f6e6cbff23778cd78c0c41e
IbnuAhsani <ibnu.muhari@gmail.com>
2020-08-31 19:46:44 +0700

    repeated string problem (100% solved)

--- repeated-string/repeated-string.java
+++ repeated-string/repeated-string.java
@@ -1,83 +1,54 @@
-    import java.io.*;
-    import java.math.*;
-    import java.security.*;
-    import java.text.*;
-    import java.util.*;
-    import java.util.concurrent.*;
-    import java.util.regex.*;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-    public class Solution {
+public class Solution {
 
-        // Complete the repeatedString function below.
-        static long repeatedString(String s, long n) {
-            
-            int stringSize = s.length();  
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        long numOfAInInputString = 0;
 
-            // early returns if n or size of string is 0
-            if(n <= 0 || stringSize <= 0)
-                return 0;
+        for(int i = 0; i <= s.length() - 1; i++)
+            if(s.charAt(i) == 'a')
+                numOfAInInputString++;
 
-            // early returns if the string only consists of a letter other than 'a'
-            if(s.charAt(0) != 'a' && s.length() == 1)
-                return 0;
+        long quotient = n / s.length();
+        long numOfAByDivision = quotient * numOfAInInputString;
 
-            // if the string only consist of the letter 'a', return the value of n
-            if(s.charAt(0) == 'a' && s.length() == 1)
-                return n;
+        long remainder = n % s.length();
+        String inputStringSubstring = s.substring(0, (int) remainder);
 
-            int arraySize = (int) n;
-            
-            char[] charArray = new char[arraySize];
+        long numOfAByModulo = 0;
+        for(int i = 0; i <= inputStringSubstring.length() - 1; i++)
+            if(inputStringSubstring.charAt(i) == 'a')
+                numOfAByModulo++;
 
-            int globalIndex = 0;
-            int stringIndex = 0;
-            
-            // loop until the global index is the size of n - 1
-            while(globalIndex < arraySize){
+        long numOfA = numOfAByDivision + numOfAByModulo;
 
-                // loop for how many characters in the given string OR
-                // until the global index is the size of n - 1 (this is done
-                // so that the new char array doesnt go over the limit of n)
-                while(globalIndex < arraySize && stringIndex < stringSize){
-
-                    // append the char of the given string at index stringIndex 
-                    // to the char array at index globalIndex
-                    charArray[globalIndex] = s.charAt(stringIndex);
-
-                    ++stringIndex;
-                    ++globalIndex;
-                }
-
-                stringIndex = 0;
-            }
-
-            int aCounter = 0;
-
-            // counts how many 'a's are in the char array
-            for(int i = 0; i < arraySize; i++)
-                if(charArray[i] == 'a')
-                    aCounter++;
-
-            return aCounter;
-        }
-
-        private static final Scanner scanner = new Scanner(System.in);
-
-        public static void main(String[] args) throws IOException {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-            String s = scanner.nextLine();
-
-            long n = scanner.nextLong();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            long result = repeatedString(s, n);
-
-            bufferedWriter.write(String.valueOf(result));
-            bufferedWriter.newLine();
-
-            bufferedWriter.close();
-
-            scanner.close();
-        }
+        return numOfA;
     }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
